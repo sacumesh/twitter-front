6a23e19d7312b9e432018767714e9c57da6515c1
Sachiththa Bandaranayake <sacume3@gmail.com>
2022-11-06 14:39:51 +0100

    :sparkles: setup new routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,22 +3,28 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { TweetComponent } from './tweet/tweet.component';
+import { TweetComponent } from './components/tweet/tweet.component';
 import { MatCardModule } from '@angular/material/card';
 import { MatDividerModule } from '@angular/material/divider';
 import { MatProgressBarModule } from '@angular/material/progress-bar';
 import { MatButtonModule } from '@angular/material/button';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatFormFieldModule } from '@angular/material/form-field';
-import { CreateTweetComponent } from './create-tweet/create-tweet.component';
+import { CreateTweetComponent } from './components/create-tweet/create-tweet.component';
 import { MatInputModule } from '@angular/material/input';
-import { EditTweetDialogComponent } from './edit-tweet-dialog/edit-tweet-dialog.component';
+import { EditTweetDialogComponent } from './dialogs/edit-tweet-dialog/edit-tweet-dialog.component';
 import { MatDialogModule } from '@angular/material/dialog';
 import { FormsModule } from '@angular/forms';
+import { MatListModule } from '@angular/material/list';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 import {
   MatSnackBarModule,
   MAT_SNACK_BAR_DEFAULT_OPTIONS,
 } from '@angular/material/snack-bar';
+import { UserFeedComponent } from './components/user-feed/user-feed.component';
+import { ScrollingModule } from '@angular/cdk/scrolling';
+import { HomeComponent } from './pages/home/home.component';
+import { MetamaskNotFoundComponent } from './pages/metamask-not-found/metamask-not-found.component';
 
 @NgModule({
   declarations: [
@@ -26,6 +32,9 @@ import {
     TweetComponent,
     CreateTweetComponent,
     EditTweetDialogComponent,
+    UserFeedComponent,
+    HomeComponent,
+    MetamaskNotFoundComponent,
   ],
   imports: [
     BrowserModule,
@@ -41,6 +50,9 @@ import {
     MatInputModule,
     MatDialogModule,
     MatSnackBarModule,
+    MatListModule,
+    MatProgressSpinnerModule,
+    ScrollingModule,
   ],
   providers: [
     {
