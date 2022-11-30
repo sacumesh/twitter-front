fa2854efb18d2f870f87d1e2253b92538809b340
Sachiththa Bandaranayake <sacume3@gmail.com>
2022-11-30 21:59:49 +0100

    :art: final

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  contractAddress: '0x8F40F03591Dfe64CbF2EC55586e43ac9e25bdaC7',
+  contractAddress: '0x7A18Ce4E6529e0f8E02149Bea1f1Bfd780a20928',
   web3WebsocketProviderHost:
     'wss://goerli.infura.io/ws/v3/45133a9e812e42528eed70722474bf4c',
 };
